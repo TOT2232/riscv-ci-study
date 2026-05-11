7fe5877c6c7d1266bb269e9e4c8d1d87d3ff84a3
TOT2232 <2088244135@qq.com>
2026-05-11 10:42:17 +0800

    Create hello_lpi4a.c

---
+++ hello_lpi4a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("--------------------------------------\n");
+    printf("Hello LicheePi 4A!\n");
+    printf("This RISC-V binary was built by GitHub CI.\n");
+    printf("--------------------------------------\n");
+    return 0;
+}
